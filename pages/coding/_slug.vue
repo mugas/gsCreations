fb0d98585cd80da5976da5506e15243990cd7767
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2021-03-12 12:11:45 +0200

    change metatags

--- pages/coding/_slug.vue
+++ pages/coding/_slug.vue
@@ -122,7 +122,7 @@ export default {
         {
           hid: 'og:image',
           property: 'og:image',
-          content: `${this.thumbnail}`,
+          content: this.thumbnail,
         },
         {
           hid: 'og:image:alt',
