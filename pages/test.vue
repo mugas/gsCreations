6c0e69c524807e42401768f18851dd013e638d68
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2021-05-19 05:32:24 +0300

    testing newhome page

--- pages/test.vue
+++ pages/test.vue
@@ -13,6 +13,61 @@
         </p>
       </div>
     </section>
+
+    <section class="services-offered">
+      <h2>Services</h2>
+      <article class="websites services-each">
+        <h3>Websites</h3>
+        <div class="websites__text">
+          <p>
+            Websites solutions that focus on your customer needs. Let's put your
+            brand online
+          </p>
+        </div>
+        <div class="websites__image">
+          <img
+            class="services__images"
+            width="300px"
+            src="~/assets/images/land__mobile.png"
+            alt=""
+          />
+        </div>
+      </article>
+      <article class="seo">
+        <h3>SEO</h3>
+        <div class="seo__image">
+          <img
+            class="services__images"
+            width="300px"
+            src="~/assets/images/land__desktop.jpg"
+            alt=""
+          />
+        </div>
+        <div class="seo__text">
+          <p>
+            Websites solutions that focus on your customer needs. Let's put your
+            brand online
+          </p>
+        </div>
+      </article>
+      <article class="maintenance">
+        <h3>Maintenance</h3>
+        <div class="maintenance__text">
+          <p>
+            Websites solutions that focus on your customer needs. Let's put your
+            brand online
+          </p>
+        </div>
+        <div class="maintenance__image">
+          <img
+            class="services__images"
+            width="300px"
+            src="~/assets/images/land__mob.jpg"
+            alt=""
+          />
+        </div>
+      </article>
+    </section>
     <section>
       <Services />
     </section>
@@ -42,25 +97,26 @@ h2 {
   font-size: 1em;
   margin-top: 7px;
 }
+h3 {
+  color: #28430a;
+}
 
 .text {
   display: flex;
   flex-direction: column;
   justify-content: center;
 }
+.services__images {
+  width: 100%;
+}
 
 /*Services*/
-.services {
+.services-offered {
   display: flex;
   flex-direction: column;
+  text-align: center;
 }
-.services__image {
-  width: 100px;
-}
-h3 {
-  color: black;
-}
-.services__description {
+.services-each {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -92,15 +148,6 @@ h3 {
     justify-content: space-around;
     align-items: center;
     text-align: center;
-  }
-
-  .services__title {
-    width: 100%;
-  }
-  .services__name {
-    display: flex;
-    justify-content: center;
-    flex-direction: row;
   }
 }
 </style>
